1c4711b24b670ac500702a964976253cfdd3099f
Jessekay <kayjes00@gmail.com>
2025-10-19 14:24:22 +0200

    modified the readme file

---
+++ spring capstone/src/main/java/com/example/Talenta/TalentaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Talenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentaApplication.class, args);
+	}
+
+}
---
+++ spring capstone/src/test/java/com/example/Talenta/TalentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Talenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
