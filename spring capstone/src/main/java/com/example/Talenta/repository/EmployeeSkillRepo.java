287b5bc73d21fb36141d5fdacd52fc3fd991aaed
Jessekay <kayjes00@gmail.com>
2025-10-25 21:26:06 +0200

    added a custom query

--- spring capstone/src/main/java/com/example/Talenta/repository/EmployeeSkillRepo.java
+++ spring capstone/src/main/java/com/example/Talenta/repository/EmployeeSkillRepo.java
@@ -3,7 +3,11 @@ package com.example.Talenta.repository;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.Talenta.model.EmployeeSkill;
 
@@ -11,4 +15,14 @@ public interface EmployeeSkillRepo extends JpaRepository<EmployeeSkill, UUID>{
     List<EmployeeSkill> findByEmployeeId(UUID employeeId);
     List<EmployeeSkill> findBySkillName(String skillName);
     List<EmployeeSkill> findByProficiencyLevel(String level);
+
+    boolean existsByEmployeeIdAndSkillId(UUID employeeId, UUID skillId);
+
+    List<EmployeeSkill> findByEmployeeIdOrderByAcquisitionDateDesc(UUID employeeId);
+
+    Page<EmployeeSkill> findByEmployeeId(UUID employeeId, Pageable pageable);
+
+    @Query("SELECT es FROM EmployeeSkill es WHERE es.skill.isCore = false AND es.skill.name = :skillName")
+    List<EmployeeSkill> findHiddenTalentHolders(@Param("skillName") String skillName);
+    
 }
